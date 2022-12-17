77090dd824a78da302f06a7a51360515a3cf4620
Artur Kamieniecki <kamart02@wp.pl>
2022-12-17 15:14:36 +0100

    Removed caching

--- sql/sql.php
+++ sql/sql.php
@@ -5,6 +5,9 @@
         href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/default.min.css">
     <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"></script>
     <script>hljs.highlightAll();</script>
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
 </head>
 
 <body>
